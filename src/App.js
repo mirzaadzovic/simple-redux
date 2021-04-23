1c1b76d7a69494332c663cdeeba5cd8d2949f493
youngrade <katarza.21@gmail.com>
2021-04-23 18:15:49 +0200

    reset_all button centered

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useSelector, useDispatch } from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import "./App.css";
 import Counter from "./components/Counter";
 import decrement from "./actions/decrement";
@@ -10,18 +10,30 @@ function App() {
     <div className="App">
       <h2>Redux</h2>
       <div className="ukupno">
-        <p>
+        <p
+          style={{
+            maxWidth: "200px",
+            margin: "0 auto",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "relative",
+          }}
+        >
           <b>Total: </b>
           {counter}
         </p>
-        {counter ? (
-          <button
-            className="btn btn-danger"
-            onClick={() => dispatch({ type: "RESET_ALL" })}
-          >
-            Reset all
-          </button>
-        ) : null}
+        <div style={{maxWidth: "200px", height: "30px", margin: "0 auto"}}>
+          {counter ? (
+            <button
+              style={{margin: "0 auto"}}
+              className="btn btn-danger"
+              onClick={() => dispatch({type: "RESET_ALL"})}
+            >
+              Reset all
+            </button>
+          ) : null}
+        </div>
       </div>
       <Counter by={1} />
       <Counter by={2} />
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import increment from "../actions/increment";
 import decrement from "../actions/decrement";
 import "./Counter.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const Counter = ({ by }) => {
+const Counter = ({by}) => {
   let counters = useSelector((state) => state.countersStates);
   const counter = counters.find((c) => c.id == by).counter;
   const dispatch = useDispatch();
@@ -17,12 +17,12 @@ const Counter = ({ by }) => {
   }
 
   function resetCounter() {
-    dispatch({ by: by, type: "RESET", counter: counter });
+    dispatch({by: by, type: "RESET", counter: counter});
   }
 
   return (
     <div className="Counter">
-      <h4>Increment/Decrement by {by}</h4>
+      <h4 style={{wordWrap: "break-word"}}>Increment/Decrement by {by}</h4>
       <p className="Counter__count">{!counter ? <b>zero</b> : counter} </p>
       <button
         className="btn btn-secondary"
